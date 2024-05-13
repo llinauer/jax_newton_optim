d74ac8add2698fed44a48b7d26d3f6b892c76018
Lukas Linauer <lukas@datenvorsprung.at>
2024-05-13 13:56:22 +0200

    Change optim.optimize to use a standard python for-loop
    Saving intermediate values with jax.while_loop is not possible

--- optim.py
+++ optim.py
@@ -11,16 +11,15 @@ from typing import Callable
 
 
 def optimize(fun: Callable, x0: ArrayLike, max_iterations: int = 100,
-        tol: float = 1e-8) -> ArrayLike:
+             tol: float = 1e-6) -> ArrayLike:
     """ Search for a local optimum of the given function, starting at x0  """
 
-    iter_count = 0
-    x_prev = jnp.zeros_like(x0)
+    x = x0
 
-    def newton_step(args):
-        # do one step in newtons method
+    x_vals = jnp.array([])
+    grads = jnp.array([])
 
-        x, _, iter_count = args
+    for _ in range(max_iterations):
 
         # calculate the first derivative (jacobian) and second derivative (hessian) of the function
         # at x0
@@ -29,28 +28,28 @@ def optimize(fun: Callable, x0: ArrayLike, max_iterations: int = 100,
     
         # calculate the step size dx to take (dx = -H⁻¹ / J)
         dx = -jnp.linalg.solve(hess, jac[:, None])[:, 0, 0]
-        iter_count += 1
+        x_new = x + dx
 
-        return x + dx, x, iter_count
+        x_vals = jnp.append(x_vals, x)
+        grads = jnp.append(grads, jac)
 
-    def stop_condition(args):
-        # check if the iteration should be stopped
-        x, x_prev, iter_count = args
+        if abs(x_new - x) < tol:
+            break
 
-        return jax.lax.bitwise_and(jnp.linalg.norm(x - x_prev) > tol, iter_count < max_iterations)
-    
-    x_opt, _, _ = jax.lax.while_loop(stop_condition, newton_step, (x0, x_prev, iter_count))
-    return x_opt
+        x = x_new
+
+    return x, x_vals, grads
 
 
-def square(x: ArrayLike) -> ArrayLike:
-    return jnp.square(x)
+def poly(x: ArrayLike) -> ArrayLike:
+    return x**3 + 2*x**2 - 3*x + 2
 
 
 def main():
 
     # optimize f(x) = x², start at 8
-    optimum = optimize(square, jnp.array([8.]))
+    x_opt, x_vals, grads = optimize(poly, jnp.array([3.]))
+    print(x_opt, x_vals, grads)
 
 
 if __name__ == '__main__':
